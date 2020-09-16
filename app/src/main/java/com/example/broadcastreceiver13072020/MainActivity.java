f85443ca050ce253f27f27c4a903aacab1b21e27
phatdroid94 <phatdroid94@gmail.com>
2020-09-16 18:43:11 +0700

    send broadcast notification

--- app/src/main/java/com/example/broadcastreceiver13072020/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver13072020/MainActivity.java
@@ -1,21 +1,69 @@
 package com.example.broadcastreceiver13072020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnSendBroadCast;
+    String CHANNEL_ID = "Mychannel";
+    int REQUEST_CODE = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        registerReceiver(
-                new MyBroadcast(),
-                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
-        );
+
+        mBtnSendBroadCast = findViewById(R.id.buttonSendBroadcast);
+
+        mBtnSendBroadCast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+            }
+        });
     }
+    private void createNotification(){
+        Intent intent = new Intent(MainActivity.this,MyBroadcast.class);
+        intent.setAction("Message");
+        intent.putExtra("message","Xin chao");
+        PendingIntent pendingIntent =
+                PendingIntent.getBroadcast(
+                        MainActivity.this,
+                        REQUEST_CODE,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(
+                        MainActivity.this,
+                        CHANNEL_ID)
+                .setContentTitle("Ban co thong bao moi")
+                .setContentText("Co tin nhan moi")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setShowWhen(true)
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "channel",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        notificationManager.notify(1,builder.build());
+    }
+
 }
--- app/src/main/java/com/example/broadcastreceiver13072020/MyBroadcast.java
+++ app/src/main/java/com/example/broadcastreceiver13072020/MyBroadcast.java
@@ -8,6 +8,14 @@ import android.widget.Toast;
 public class MyBroadcast extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Running", Toast.LENGTH_SHORT).show();
+//        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED) {
+//            Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+//        }else{
+//            Intent intent1 =new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+//            context.startActivity(intent1);
+//        }
+
+        Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
     }
 }
